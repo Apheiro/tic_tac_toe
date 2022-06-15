e9611de38f835b7feeb92546fdf07ce21573ae8a
Eyen <eyentuyon@gmail.com>
2022-06-15 16:35:30 -0300

    Add animation and fix bugs.

--- script.js
+++ script.js
@@ -11,6 +11,12 @@ const player1Name = document.querySelector('#player1Name');
 const player2Name = document.querySelector('#player2Name');
 const player1Score = document.querySelector('#player1Score');
 const player2Score = document.querySelector('#player2Score');
+const gameOver = document.querySelector('#gameOver');
+const gameOverResult = document.querySelector('#gameOverResult');
+const playAgain = document.querySelector('#playAgain');
+const backToMenuBtn = document.querySelector('#backToMenuBtn');
+const btnRefresh = document.querySelector('#btnRefresh');
+const btnConfig = document.querySelector('#btnConfig');
 
 class player{
     constructor(symbol, name, score, turn){
@@ -33,8 +39,26 @@ class game{
 
     fillCellsOnBoard(){
         boardCell.forEach(boardCell => {
-            boardCell.textContent = `${gameSession.boardData[boardCell.attributes[0].value]}`;
+            boardCell.innerText = `${gameSession.boardData[boardCell.attributes[0].value]}`;
         });
+    }
+
+    fillBoardData() { 
+        if (!this.innerText) {
+            gameSession.turnIndex++;
+            this.classList.add('gameBoardCellAnimation');
+            if (gameSession.turnIndex % 2 === 0) {
+                gameSession.boardData[this.attributes[0].value] = player1.symbol;
+                player1Name.classList.toggle('playersNameActive');
+                player2Name.classList.toggle('playersNameActive');
+            } else {
+                gameSession.boardData[this.attributes[0].value] = player2.symbol;
+                player1Name.classList.toggle('playersNameActive');
+                player2Name.classList.toggle('playersNameActive');
+            }
+        }
+        gameSession.fillCellsOnBoard();
+        gameSession.gameResult();
     }
 
     gameResult() {
@@ -56,7 +80,9 @@ class game{
                 player2Score.innerText = player2.score;
             }
             gameSession.round++;
-            setTimeout(() => {gameSession.resetRound();}, 500);
+            boardCell.forEach(boardCell => {boardCell.style = 'pointer-events: none;'});
+            setTimeout(() => { gameSession.resetRound(); }, 1000);
+            setTimeout(() => {gameSession.gameOver();}, 1000);
         } else if (gameSession.boardData[0] === 'O' && gameSession.boardData[1] === gameSession.boardData[0] && gameSession.boardData[2] === gameSession.boardData[0] ||
             gameSession.boardData[3] === 'O' && gameSession.boardData[4] === gameSession.boardData[3] && gameSession.boardData[5] === gameSession.boardData[3] ||
             gameSession.boardData[6] === 'O' && gameSession.boardData[7] === gameSession.boardData[6] && gameSession.boardData[8] === gameSession.boardData[6] ||
@@ -75,33 +101,37 @@ class game{
                 player2Score.innerText = player2.score;
             }
             gameSession.round++;
-            setTimeout(() => {gameSession.resetRound();}, 500);
+            boardCell.forEach(boardCell => {boardCell.style = 'pointer-events: none;'});
+            setTimeout(() => { gameSession.resetRound(); }, 1000);
+            setTimeout(() => {gameSession.gameOver();}, 1000);
         } else if (gameSession.boardData.includes('') === false) {
             round.textContent = `It's a draw!`;
             gameSession.round++;
-            setTimeout(() => {gameSession.resetRound();}, 500);
-        }
-        if (gameSession.round > gameSession.maxRounds) {
-            if (player1.score > player2.score) { alert(`${player1.name} won!`); }
-            else if (player1.score === player2.score) { alert(`TIE!`); }
-            else { alert(`${player2.name} won!`); };
-            gameSession.resetGame();
-        }
+            boardCell.forEach(boardCell => {boardCell.style = 'pointer-events: none;'});
+            setTimeout(() => { gameSession.resetRound(); }, 1000);
+            setTimeout(() => {gameSession.gameOver();}, 1000);
+        }  
     }
 
-    fillBoardData() { 
-        if (!this.innerText) {
-            gameSession.turnIndex++;
-            if (gameSession.turnIndex % 2 === 0) {
-            gameSession.boardData[this.attributes[0].value] = player1.symbol;
-            } else {
-            gameSession.boardData[this.attributes[0].value] = player2.symbol;
+    gameOver() {
+        if (gameSession.round > gameSession.maxRounds) {
+            playGame.classList.toggle('animationFadeOut');
+            setTimeout(() => {
+                playGame.classList.toggle('animationFadeOut');
+                playGame.classList.toggle('hidden')
+                gameOver.classList.toggle('hidden')
+            }, 490);
+            
+            if (player1.score > player2.score) {
+                gameOverResult.textContent = `${player1.name} won!`;
             }
-        } else {
-            console.log('not empty');
+            else if (player1.score === player2.score) {
+                gameOverResult.textContent = `It's a draw!`;
+            }
+            else { 
+                gameOverResult.textContent = `${player2.name} won!`;
+            };
         }
-        gameSession.fillCellsOnBoard();
-        gameSession.gameResult();
     }
 
     startGame() {
@@ -111,10 +141,20 @@ class game{
         const player1NameInput = document.querySelector('#player1NameInput').value;
         const player2NameInput = document.querySelector('#player2NameInput').value;
 
-        if (!startFirst.checked) {
+        if (startFirst.checked) {
+            player1.turn = true;
+            player2.turn = false;
+            gameSession.turnIndex = 1;
+            player1Name.classList.toggle('playersNameActive');
+            player1Name.classList.add('playersNameActive');
+            player2Name.classList.remove('playersNameActive');
+        } else {
             player1.turn = false;
             player2.turn = true;
-        } 
+            gameSession.turnIndex = 0;
+            player2Name.classList.add('playersNameActive');
+            player1Name.classList.remove('playersNameActive');
+        }
 
         if (IA.checked) {
             player2.name = 'IA';
@@ -123,6 +163,9 @@ class game{
         if (symbol.checked) {
             player1.symbol = 'O';
             player2.symbol = 'X';
+        } else {
+            player1.symbol = 'X';
+            player2.symbol = 'O';
         }
 
         if (player1NameInput !== "") { 
@@ -138,13 +181,6 @@ class game{
         } else {
             player2Name.innerText = player2.name;
         }
-        
-        if (player1.turn === true) { 
-            gameSession.turnIndex = 1;
-        }
-        
-        settings.classList.toggle('hidden');
-        playGame.classList.toggle('hidden');
     }
 
     resetRound() { 
@@ -153,32 +189,78 @@ class game{
         round.innerText = `ROUND ${gameSession.round}`;
         if (player1.turn === true) { 
             gameSession.turnIndex = 1;
+            player1Name.classList.toggle('playersNameActive');
+            player2Name.classList.toggle('playersNameActive');
         } else {
             gameSession.turnIndex = 0;
+            player1Name.classList.toggle('playersNameActive');
+            player2Name.classList.toggle('playersNameActive');
         }
+        boardCell.forEach(boardCell => {
+            boardCell.classList.remove('gameBoardCellAnimation');
+            boardCell.style = 'pointer-events: auto;';
+        });
     }
 
     resetGame() {
-        gameSession.round = 1;
+        if (player1.turn === true) { 
+            gameSession.turnIndex = 1;
+        } else if (player2.turn === true) {
+            gameSession.turnIndex = 0;
+        }
         player1.score = 0;
         player2.score = 0;
         player1Score.innerText = player1.score;
         player2Score.innerText = player2.score;
+        gameSession.round = 1;
         gameSession.boardData = ['', '', '', '', '', '', '', '', ''];
         gameSession.fillCellsOnBoard();
         round.innerText = `ROUND ${gameSession.round}`;
     }
-    
+
 }
+
+
 const player1 = new player('X', 'Player-1', 0, true);
 const player2 = new player('O', 'Player-2', 0, false);
-const gameSession = new game(player1, player2)
+const gameSession = new game(player1, player2);
 
 presentation.onclick = () => {
-    presentation.classList.toggle('hidden');
-    settings.classList.toggle('hidden');
+    presentation.classList.toggle('animationFadeOut');
+    setTimeout(() => {
+        presentation.classList.toggle('animationFadeOut');
+        presentation.classList.toggle('hidden');
+        settings.classList.toggle('hidden');
+    }, 490);
+    
 }
-
+startBtn.onclick = () => {
+    gameSession.startGame();
+    settings.classList.toggle('animationFadeOut');
+    setTimeout(() => {
+        settings.classList.toggle('animationFadeOut');
+        settings.classList.toggle('hidden');
+        playGame.classList.toggle('hidden');
+    }, 490);
+}
+playAgain.onclick = () => {
+    gameOver.classList.toggle('animationFadeOut');
+    setTimeout(() => {
+        gameSession.resetGame();
+        gameOver.classList.toggle('animationFadeOut');
+        playGame.classList.toggle('hidden')
+        gameOver.classList.toggle('hidden')
+    }, 490);
+}
+backToMenuBtn.onclick = () => {
+    gameOver.classList.toggle('animationFadeOut');
+    setTimeout(() => {
+        gameSession.resetGame();
+        gameOver.classList.toggle('animationFadeOut');
+        gameOver.classList.toggle('hidden')
+        settings.classList.toggle('hidden')
+    }, 490);
+}
 increaseRound.onclick = () => {
     if (gameSession.maxRounds < 9) { 
         gameSession.maxRounds++;
@@ -188,7 +270,6 @@ increaseRound.onclick = () => {
         roundsNumber.textContent = gameSession.maxRounds;
     }
 }
-
 decreaseRound.onclick = () => {
     if (gameSession.maxRounds > 1 && gameSession.maxRounds < 11) { 
         gameSession.maxRounds--;
@@ -198,6 +279,18 @@ decreaseRound.onclick = () => {
         roundsNumber.textContent = gameSession.maxRounds;
     }
 }
+btnConfig.onclick = () => {
+    playGame.classList.toggle('animationFadeOut');
+    setTimeout(() => {
+        gameSession.resetGame();
+        playGame.classList.toggle('animationFadeOut');
+        playGame.classList.toggle('hidden')
+        settings.classList.toggle('hidden')
+    }, 490);
+}
+btnRefresh.onclick = () => {
+    
+    gameSession.resetGame();
+}
 
-startBtn.onclick = gameSession.startGame;
-boardCell.forEach(boardCell => boardCell.addEventListener('click', gameSession.fillBoardData));
+boardCell.forEach(boardCell => boardCell.onclick =  gameSession.fillBoardData);
